1ba4de0ff5d0a0c7cfd03eccdccaa1ebf4ca6b9e
Iaro <iaroslav.slovak@gmail.com>
2021-06-18 18:06:40 +0300

    Initial commit

---
+++ src/main/java/ro/fasttrackit/homework18/countries/Countries.java
@@ -0,0 +1,80 @@
+package ro.fasttrackit.homework18.countries;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Countries {
+    private final int id;
+    private final String name;
+    private final String capital;
+    private final long population;
+    private final long area;
+    private final String continent;
+    private final List<String> neighbours;
+
+    public Countries(int id, String name, String capital, long population, long area, String continent, List<String> neighbours) {
+        this.id = id;
+        this.name = name;
+        this.capital = capital;
+        this.population = population;
+        this.area = area;
+        this.continent = continent;
+        this.neighbours = neighbours;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public long getArea() {
+        return area;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public List<String> getNeighbours() {
+        return Collections.unmodifiableList(neighbours);
+    }
+
+    @Override
+    public String toString() {
+        return "Countries{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", capital='" + capital + '\'' +
+                ", population=" + population +
+                ", area=" + area +
+                ", continent='" + continent + '\'' +
+                ", neighbours=" + neighbours +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Countries)) return false;
+        Countries countries = (Countries) o;
+        return getId() == countries.getId() && getPopulation() == countries.getPopulation() && getArea() == countries.getArea() && Objects.equals(getName(), countries.getName()) && Objects.equals(getCapital(), countries.getCapital()) && Objects.equals(getContinent(), countries.getContinent()) && Objects.equals(getNeighbours(), countries.getNeighbours());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getCapital(), getPopulation(), getArea(), getContinent(), getNeighbours());
+    }
+}
+
--- src/main/java/ro/fasttrackit/homework18/countries/CountryReader.java
+++ src/main/java/ro/fasttrackit/homework18/countries/CountryReader.java
@@ -1,4 +1,47 @@
 package ro.fasttrackit.homework18.countries;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CountryReader {
+    public static void main(String[] args) throws Exception {
+        List<Countries> countries = readCountriesFromFile();
+        countries.forEach(System.out::println);
+    }
+
+    private static List<Countries> readCountriesFromFile() throws Exception {
+        List<Countries> countries = new ArrayList<>();
+        BufferedReader fileReader = new BufferedReader(new FileReader("files/countries"));
+        int id = 1;
+        String line;
+        while ((line = fileReader.readLine()) != null) {
+            countries.add(getCountries(id++, line));
+        }
+        return countries;
+    }
+
+    private static Countries getCountries(int id, String countryInfo) {
+        String[] countryData = countryInfo.split("\\|");
+        List<String> neighbours = new ArrayList<>();
+        if (countryData.length > 5) {
+            neighbours = getNeighbours(countryData[5]);
+        }
+        return new Countries(id,
+                countryData[0],
+                countryData[1],
+                Long.parseLong(countryData[2]),
+                Long.parseLong(countryData[3]),
+                countryData[4],
+                neighbours);
+    }
+
+    private static List<String> getNeighbours(String neighbours) {
+        String[] neighboursList = neighbours.split("[~]");
+        List<String> result = new ArrayList<>();
+        Collections.addAll(result, neighboursList);
+        return result;
+    }
 }
